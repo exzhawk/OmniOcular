29fe1a553f87b846acf68913bd94cdb028c9778d
Epix <git@exz.me>
2014-10-20 01:32:20 +0800

    initial mod file

---
+++ src/main/java/me/exz/omniocular/reference/Reference.java
@@ -0,0 +1,7 @@
+package me.exz.omniocular.reference;
+
+public class Reference {
+    public static final String MOD_ID = "OmniOcular";
+    public static final String MOD_NAME = "Omni Ocular";
+    public static final String VERSION = "@VERSION@";
+}
