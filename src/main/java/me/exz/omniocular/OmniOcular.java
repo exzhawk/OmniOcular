d26313220cdf06f8b6e25bdeb01d07caee3f0b3c
Epix <git@exz.me>
2014-10-20 14:38:41 +0800

    git derps

--- src/main/java/me/exz/omniocular/OmniOcular.java
+++ src/main/java/me/exz/omniocular/OmniOcular.java
@@ -1,1 +1,11 @@
-233
+package me.exz.omniocular;
+
+import cpw.mods.fml.common.Mod;
+import me.exz.omniocular.reference.Reference;
+
+//@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:Waila;required-after:NotEnoughItems")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "")
+
+public class OmniOcular {
+
+}
