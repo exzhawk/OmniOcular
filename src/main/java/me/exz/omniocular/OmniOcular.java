cd871c411689fa19fad3c2a265f3d424018da5be
Epix <git@exz.me>
2014-10-21 01:29:33 +0800

    update forge and gradle config

--- src/main/java/me/exz/omniocular/OmniOcular.java
+++ src/main/java/me/exz/omniocular/OmniOcular.java
@@ -3,8 +3,7 @@ package me.exz.omniocular;
 import cpw.mods.fml.common.Mod;
 import me.exz.omniocular.reference.Reference;
 
-//@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:Waila;required-after:NotEnoughItems")
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:Waila;required-after:NotEnoughItems")
 
 public class OmniOcular {
 
