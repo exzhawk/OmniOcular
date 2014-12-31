9d3d369c37362a554b30089f0bf95c08782faa40
Epix <git@exz.me>
2015-01-01 03:17:07 +0800

    update to waila new api

--- src/main/java/me/exz/omniocular/handler/EntityHandler.java
+++ src/main/java/me/exz/omniocular/handler/EntityHandler.java
@@ -6,7 +6,9 @@ import mcp.mobius.waila.api.IWailaEntityProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -14,8 +16,10 @@ public class EntityHandler implements IWailaEntityProvider {
     @SuppressWarnings("UnusedDeclaration")
     public static void callbackRegister(IWailaRegistrar registrar) {
         EntityHandler instance = new EntityHandler();
-        registrar.registerSyncedNBTKey("*", Entity.class);
+//        registrar.registerSyncedNBTKey("*", Entity.class);
         registrar.registerBodyProvider(instance, Entity.class);
+        registrar.registerNBTProvider(instance, Entity.class);
+
     }
 
     @Override
@@ -40,5 +44,13 @@ public class EntityHandler implements IWailaEntityProvider {
     @Override
     public List<String> getWailaTail(Entity entity, List<String> currenttip, IWailaEntityAccessor accessor, IWailaConfigHandler config) {
         return currenttip;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, Entity ent, NBTTagCompound tag, World world) {
+        if (ent != null)
+            ent.writeToNBT(tag);
+        return tag;
+
     }
 }
--- src/main/java/me/exz/omniocular/handler/TileEntityHandler.java
+++ src/main/java/me/exz/omniocular/handler/TileEntityHandler.java
@@ -5,9 +5,11 @@ import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -16,9 +18,11 @@ public class TileEntityHandler implements IWailaDataProvider {
     @SuppressWarnings("UnusedDeclaration")
     public static void callbackRegister(IWailaRegistrar registrar) {
         TileEntityHandler instance = new TileEntityHandler();
-        registrar.registerSyncedNBTKey("*", Block.class);
-        registrar.registerSyncedNBTKey("*", TileEntity.class);
+//        registrar.registerSyncedNBTKey("*", Block.class);
+//        registrar.registerSyncedNBTKey("*", TileEntity.class);
         registrar.registerBodyProvider(instance, Block.class);
+        registrar.registerNBTProvider(instance, Block.class);
+//        registrar.registerNBTProvider(instance, TileEntity.class);
 
     }
 
@@ -45,5 +49,12 @@ public class TileEntityHandler implements IWailaDataProvider {
     @Override
     public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
         return currenttip;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x, int y, int z) {
+        if (te != null)
+            te.writeToNBT(tag);
+        return tag;
     }
 }
