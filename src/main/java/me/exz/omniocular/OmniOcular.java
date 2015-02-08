76a1d9d6bde21c60ec3cda1d859c33e1f6e19593
Epix <git@exz.me>
2015-02-09 00:10:19 +0800

    require mod installed on server side. fixed #1

--- src/main/java/me/exz/omniocular/OmniOcular.java
+++ src/main/java/me/exz/omniocular/OmniOcular.java
@@ -6,8 +6,12 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
 import me.exz.omniocular.proxy.IProxy;
 import me.exz.omniocular.reference.Reference;
+
+import java.util.Map;
 
 @SuppressWarnings({"UnusedParameters", "UnusedDeclaration"})
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:Waila;required-after:NotEnoughItems")
@@ -40,6 +44,10 @@ public class OmniOcular {
     void onServerStart(FMLServerStartingEvent event) {
 //        LogHelper.info("FMLServerStartingEvent");
         proxy.registerServerCommand(event);
+    }
+    @NetworkCheckHandler
+    public static boolean check(Map<String,String> remote,Side side){
+        return !(side == Side.SERVER && !remote.isEmpty() && !remote.containsKey(Reference.MOD_ID));
     }
 
 
